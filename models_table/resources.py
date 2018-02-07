655799d41bd1f7698f75b8ddf83b8af47ee02275
Humboldt155 <petr_kozyrev@icloud.com>
2018-02-07 17:25:07 +0300

    Update resources.py

--- models_table/resources.py
+++ models_table/resources.py
@@ -13,44 +13,62 @@ from .models import \
 class DepartmentResource(resources.ModelResource):
     class Meta:
         model = Department
+        skip_unchanged = True
+        report_skipped = False
 
 
 class ModelResource(resources.ModelResource):
     class Meta:
         model = Model
+        skip_unchanged = True
+        report_skipped = False
 
 
 class AttributeResource(resources.ModelResource):
     class Meta:
         model = Attribute
+        skip_unchanged = True
+        report_skipped = False
 
 
 class ValueResource(resources.ModelResource):
     class Meta:
         model = Value
+        skip_unchanged = True
+        report_skipped = False
 
 
 class LMCodeResource(resources.ModelResource):
     class Meta:
         model = LMCode
+        skip_unchanged = True
+        report_skipped = False
 
 
 class LinkResource(resources.ModelResource):
     class Meta:
         model = Link
+        skip_unchanged = True
+        report_skipped = False
 
 
 class DepartmentAdeoResource(resources.ModelResource):
     class Meta:
         model = DepartmentAdeo
+        skip_unchanged = True
+        report_skipped = False
 
 
 class SubDepartmentAdeoResource(resources.ModelResource):
     class Meta:
         model = SubDepartmentAdeo
+        skip_unchanged = True
+        report_skipped = False
 
 
 class ModelGroupAdeoResource(resources.ModelResource):
     class Meta:
         model = ModelGroupAdeo
+        skip_unchanged = True
+        report_skipped = False
 
